ebff26fa04bf1e10d2befca5f596f324efb6ed5f
Sergio Ortega <instintoimagen@gmail.com>
2024-08-30 17:43:53 -0300

    Td004 - eslint fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -35,6 +35,7 @@ export default function Main() {
 
   useEffect(() => {
     (async () => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
       setPlace(await useInitialLocation());
     })();
   }, []);
--- src/helpers/useInitialLocation.ts
+++ src/helpers/useInitialLocation.ts
@@ -6,6 +6,7 @@ export default async function useInitialLocation(): Promise<string> {
   if (savedLocation !== null) {
     return savedLocation;
   } else {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const location = await useGetGPSLocation();
     localStorage.setItem("location", location);
     return location;
